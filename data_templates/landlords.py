6f3d179f97cff0212eeb217fd032f2c9d76bd520
Cyprien Chasseray <cyprien.chasseray@gmail.com>
2023-01-21 17:58:32 +0100

    updated db schemas

--- data_templates/landlords.py
+++ data_templates/landlords.py
@@ -3,21 +3,15 @@ landlords_model = {
         "first_name" : "",
         "last_name" : "",
         "date_of_birth": "",
-        "adress": {
-                    "country": "",
-                    "city": "",
-                    "zip_code": "",
-                    "street": "",
-                    "street_number": "",
-                    "building number": "",
-                    "apartment_number": "",
-                    "floor": "",
-                    "other_specification": ""
-                   },
-        "contact_information": {
-                                "phone_number": {"prefix": "", "number": ""},
-                                "email": "",
-                                },
-        "average_ratings": "",
-        "properties_id_list": [""],
-        }
+        "country": "",
+        "city": "",
+        "zip_code": "",
+        "street": "",
+        "street_number": "",
+        "building number": "",
+        "apartment_number": "",
+        "floor": "",
+        "other_specification": "",
+        "phone_number": "",
+        "email": "",
+        "average_ratings": ""}
